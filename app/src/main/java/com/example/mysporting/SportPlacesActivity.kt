acd2fdf10bce0f409c627eb10cc23ea844f2c09f
Réka Domján <reka.domjan@gmail.com>
2022-04-10 16:08:40 +0200

    add navigation from main page and create necessary activities

---
+++ app/src/main/java/com/example/mysporting/SportPlacesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mysporting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SportPlacesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sport_places)
+    }
+}
